158f6381d4254c7b4cb3985835e6a86958bb1097
scrappydew <scrappy@mm.st>
2021-10-29 21:19:06 -0500

    Formatting

--- src/views/Help/Help.tsx
+++ src/views/Help/Help.tsx
@@ -9,28 +9,24 @@ const Help: React.FC = () => {
   const { darkMode } = useTheme();
   return (
     <Page>
-      <PageHeader
-        icon=""
-        subtitle={"See below for farming instructions!"}
-        title="Tentacle.Finance help"
-      />
+      <PageHeader icon="" subtitle={"See below for farming instructions!"} title="Tentacle.Finance help" />
       <ol>
-       <li>Obtain UBQ and INK</li>
-       <StyledLink href="https://bittrex.com/Market/Index?MarketName=USDT-UBQ" target="_blank">
-         Buy UBQ
-       </StyledLink>
-       <StyledLink href="https://twitter.com/ubiqsmart" target="_blank">
-         Buy INK
-       </StyledLink>
-       <li>Provide UBQ and INK as LP on Shinobi. Note: You will need to "Approve" first on the same page.</li>
-       <StyledLink href="https://shinobi-info.ubiq.ninja" target="_blank">
-         Provide LP
-       </StyledLink>
-       <li>Stake your LP tokens on the Tentacle.Finance farm page</li>
-       <StyledLink href="farm" target="_blank">
-         Stake
-       </StyledLink>
-       <li>Every block your INK balance should now increase!</li>
+        <li>Obtain UBQ and INK</li>
+        <StyledLink href="https://bittrex.com/Market/Index?MarketName=USDT-UBQ" target="_blank">
+          Buy UBQ
+        </StyledLink>
+        <StyledLink href="https://twitter.com/ubiqsmart" target="_blank">
+          Buy INK
+        </StyledLink>
+        <li>Provide UBQ and INK as LP on Shinobi. Note: You will need to "Approve" first on the same page.</li>
+        <StyledLink href="https://shinobi-info.ubiq.ninja" target="_blank">
+          Provide LP
+        </StyledLink>
+        <li>Stake your LP tokens on the Tentacle.Finance farm page</li>
+        <StyledLink href="farm" target="_blank">
+          Stake
+        </StyledLink>
+        <li>Every block your INK balance should now increase!</li>
       </ol>
     </Page>
   );
