ec00cc354a63d78f3a7c92f517d35a2a1a75832c
scrappydew <scrappy@mm.st>
2021-11-19 23:54:15 -0600

    basic home,help page mui conversion

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -14,62 +14,124 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 
-function BasicChips() {
+import Tooltip from '@mui/material/Tooltip';
+
+function TopBarElements() {
   return (
-    <Stack direction="row" spacing={1}>
-      <Chip label="Chip Filled" />
-      <Chip label="Chip Outlined" variant="outlined" />
+    <Stack direction="row" spacing={2}>
+      <BlockHeightChip />
     </Stack>
   );
+}
+
+function BlockHeightChip(){
+    return(
+        <Tooltip title='current block height'>
+          <Chip label="Chip Outlined" variant="outlined" color="success" />
+        </Tooltip>
+    );
+}
+
+function TimelinePhase1() {
+    return (
+      <>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+      <Tooltip title='phase 1'>
+        <TimelineContent>
+          <p>Phase 1</p>
+          Launch INK, Launch Tentacle.Finance Site, Begin Farming
+        </TimelineContent>
+      </Tooltip>
+      </TimelineItem>
+      </>
+);
+}
+
+function TimelinePhase2() {
+    return (
+      <>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+      <Tooltip title='phase 2'>
+        <TimelineContent>
+          <p>Phase 2</p>
+          Launch Additional Farms, Enhance Farm UI
+        </TimelineContent>
+      </Tooltip>
+      </TimelineItem>
+      </>
+    );
+}
+
+function TimelinePhase3() {
+    return (
+      <>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+      <Tooltip title='phase 3'>
+        <TimelineContent>
+          <p>Phase 3</p>
+          Additional Farming Features, Secret Launch, Governance, Treasury, Bridges after voting
+        </TimelineContent>
+      </Tooltip>
+      </TimelineItem>
+      </>
+    );
+}
+
+function TimelinePhase4() {
+    return (
+        <>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+        <Tooltip title='phase 4'>
+          <TimelineContent>
+            <p>Phase 4</p>
+            TBA
+          </TimelineContent>
+        </Tooltip>
+        </TimelineItem>
+        </>
+    )
 }
 
 const Home: React.FC = () => {
   const { darkMode } = useTheme();
   return (
     <>
-      <BasicChips />
-      <AlternateTimeline />
       <Page>
         <PageHeader
           icon={darkMode ? "🌚" : "🌞"}
           subtitle={darkMode ? "🤫 shhh... the squids are sleeping." : "It's a great day to farm INK!"}
           title="Welcome to Tentacle.Finance"
         />
+        <RoadmapTimeline />
+        <TopBarElements />
       </Page>
     </>
   );
 };
 
-function AlternateTimeline() {
+function RoadmapTimeline() {
   return (
     <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
-      </TimelineItem>
+      <TimelinePhase1 />
+      <TimelinePhase2 />
+      <TimelinePhase3 />
+      <TimelinePhase4 />
     </Timeline>
   );
 }
