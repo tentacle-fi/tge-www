a7dac12c3928871ea0eafd3f24dc8575b5613f0e
scrappydew <scrappy@mm.st>
2021-11-27 17:25:02 -0600

    lint

--- src/views/Farm/components/Stake/Stake.tsx
+++ src/views/Farm/components/Stake/Stake.tsx
@@ -13,7 +13,7 @@ import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Link from '@mui/material/Link';
+import Link from "@mui/material/Link";
 
 interface StakeProps {
   farmKey: number;
@@ -113,11 +113,12 @@ const Stake: React.FC<StakeProps> = ({ children, farmKey }) => {
         />
       </Grid>
       <div>
-      <Link target="_blank" rel="noreferrer" href={AvailableFarms[farmKey].lp.url}>
-        <Button sx={{ width: "100%", marginTop: "10px" }} variant="outlined">
-          Manage {AvailableFarms[farmKey].name} liquidity on Shinobi<LinkIcon />
-        </Button>
-      </Link>
+        <Link target="_blank" rel="noreferrer" href={AvailableFarms[farmKey].lp.url}>
+          <Button sx={{ width: "100%", marginTop: "10px" }} variant="outlined">
+            Manage {AvailableFarms[farmKey].name} liquidity on Shinobi
+            <LinkIcon />
+          </Button>
+        </Link>
       </div>
 
       {typeof farmingStartTime !== "undefined" && farmingStartTime[farmKey] > Date.now() && (
