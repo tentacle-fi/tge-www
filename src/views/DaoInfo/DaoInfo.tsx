1b918e8948f60f96c3ab9794f1f5df227aabd5e8
scrappydew <scrappy@mm.st>
2021-12-18 12:26:16 -0600

    add a "StatsRibbon" component which can contain some DAO information

--- src/views/DaoInfo/DaoInfo.tsx
+++ src/views/DaoInfo/DaoInfo.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-
 import Page from "components/Page";
 import PageHeader from "components/PageHeader";
 import BarChartIcon from "@mui/icons-material/BarChart";
+import StatsRibbon from "./components/StatsRibbon";
 
 const subtitleText = "Various DAO stats to more easily analyze the Tentacle Finance ecosystem at a glance.";
 
 const DaoInfo: React.FC = () => {
   return (
     <Page>
-      <PageHeader icon={<BarChartIcon sx={{ fontSize: "98px" }} />} title={"DAOInfo"} subtitle={subtitleText} />
+      <PageHeader icon={<BarChartIcon sx={{ fontSize: "98px" }} />} title={"DAO Infoformation"} subtitle={subtitleText} />
+      <StatsRibbon />
     </Page>
   );
 };
---
+++ src/views/DaoInfo/components/StatsRibbon/StatsRibbon.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import styled from "styled-components";
+// import { bnToDec, getShortDisplayBalance } from "utils";
+import Typography from "@mui/material/Typography";
+// import Tooltip from "@mui/material/Tooltip";
+// import useUbiq from "hooks/useUbiq";
+
+interface StatsRibbonProps {
+  blockHeight?: string;
+}
+
+const CIRCULATINGINKPLACEHOLDER = "1,000,000";
+const ECOSYSTEMTVLPLACEHOLDER = "$375,000";
+const DAILYVOLUMEPLACEHOLDER = "$65,039";
+const DAILYTXVOLUMEPLACEHOLDER = "400";
+
+const StatsRibbon: React.FC<StatsRibbonProps> = ({ blockHeight }) => {
+  console.log("blockHeight passed: ", blockHeight);
+
+  return (
+    <>
+      <Typography variant="h4">Stats Ribbon Example</Typography>
+      <StyledStack direction="row" spacing={10}>
+        <Chip label={"Circulating INK: " + CIRCULATINGINKPLACEHOLDER} color="primary" />
+        <Chip label={"Ecosystem TVL: " + ECOSYSTEMTVLPLACEHOLDER} color="secondary" />
+        <Chip label={"24hr Vol: " + DAILYVOLUMEPLACEHOLDER} color="success" />
+        <Chip label={"24hr TXs: " + DAILYTXVOLUMEPLACEHOLDER} color="info" />
+      </StyledStack>
+    </>
+  );
+};
+
+const StyledStack = styled(Stack)`
+  borderradius: 100px;
+  background: purple;
+  padding: 20px;
+`;
+
+export default React.memo(StatsRibbon);
