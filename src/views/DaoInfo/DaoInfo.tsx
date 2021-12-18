b77488a10553d28152b65e5dcfe9d7bccd592033
scrappydew <scrappy@mm.st>
2021-12-18 10:37:39 -0600

    lint

--- src/views/DaoInfo/DaoInfo.tsx
+++ src/views/DaoInfo/DaoInfo.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import Page from "components/Page";
 import PageHeader from "components/PageHeader";
-import BarChartIcon from '@mui/icons-material/BarChart';
+import BarChartIcon from "@mui/icons-material/BarChart";
 
-const subtitleText = "Various DAO stats to more easily analyze the Tentacle Finance ecosystem at a glance."
+const subtitleText = "Various DAO stats to more easily analyze the Tentacle Finance ecosystem at a glance.";
 
 const DaoInfo: React.FC = () => {
   return (
     <Page>
-      <PageHeader icon={<BarChartIcon sx={{ fontSize: "98px"}} />} title={"DAOInfo"} subtitle={subtitleText} />
+      <PageHeader icon={<BarChartIcon sx={{ fontSize: "98px" }} />} title={"DAOInfo"} subtitle={subtitleText} />
     </Page>
   );
 };
