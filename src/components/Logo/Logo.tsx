f3080a9a814eab57f9010bad7769ad67eb955b59
Tentacle Fi <tentacle@mm.st>
2022-01-19 17:22:01 -0600

    small logo change

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import octoLogo from "assets/octo_purple.png";
 const Logo: React.FC = () => {
   return (
     <StyledLogo to="/">
-      <img src={octoLogo} alt="Tentacle Finance Logo" style={{ width: 76 }} />
+      <img src={octoLogo} alt="Tentacle Finance Logo" style={{ width: 55 }} />
 
       <StyledText>Tentacle.Finance</StyledText>
     </StyledLogo>
@@ -14,12 +14,13 @@ const Logo: React.FC = () => {
 };
 
 const StyledLogo = styled(Link)`
-  align-items: center;
+  align-items: left;
   display: flex;
-  justify-content: center;
-  margin: 0 0 0 55px;
+  flex-direction: column;
+  justify-content: left;
+  margin: 0 0 0 15px;
   min-height: 44px;
-  min-width: 76px;
+  min-width: 55px;
   padding: 0;
   text-decoration: none;
 
