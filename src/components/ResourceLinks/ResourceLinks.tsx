76996e3a1098fe7026a4ed306f7f4eb5b598106d
scrappydew <scrappy@mm.st>
2022-02-19 11:13:43 -0600

    update comment

--- src/components/ResourceLinks/ResourceLinks.tsx
+++ src/components/ResourceLinks/ResourceLinks.tsx
@@ -1,12 +1,16 @@
-import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-// Sets the menu items
-const options = ["Resources", "Nomics", "Test Entry"];
+// Sets the menu items (element zero is the menu's nav label)
+const options = [
+  'Resources',
+  'Nomics',
+  'Test Entry',
+];
 
 const ResourceLinks = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -16,7 +20,10 @@ const ResourceLinks = () => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLElement>,
+    index: number,
+  ) => {
     setSelectedIndex(index);
     setAnchorEl(null);
   };
@@ -27,17 +34,23 @@ const ResourceLinks = () => {
 
   return (
     <div>
-      <List component="nav" aria-label="Resources" sx={{ bgcolor: "white" }}>
+      <List
+        component="nav"
+        aria-label="Resources"
+        sx={{ bgcolor: 'white' }}
+      >
         <ListItem
-          // setting bgcolor here causes a slight strobe color effect on menu click
+        // setting bgcolor here causes a slight strobe color effect on menu click
           button
           id="lock-button"
           aria-haspopup="listbox"
           aria-controls="lock-menu"
-          aria-expanded={open ? "true" : undefined}
+          aria-expanded={open ? 'true' : undefined}
           onClick={handleClickListItem}
         >
-          <ListItemText secondary={options[selectedIndex]} />
+          <ListItemText
+            secondary={options[selectedIndex]}
+          />
         </ListItem>
       </List>
       <Menu
@@ -46,8 +59,8 @@ const ResourceLinks = () => {
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          "aria-labelledby": "lock-button",
-          role: "listbox",
+          'aria-labelledby': 'lock-button',
+          role: 'listbox',
         }}
       >
         {options.map((option, index) => (
@@ -64,6 +77,7 @@ const ResourceLinks = () => {
       </Menu>
     </div>
   );
-};
+}
+
 
 export default React.memo(ResourceLinks);
