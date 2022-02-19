82e70ee212d3f01092e8d306fd2e1d1a7883e268
scrappydew <scrappy@mm.st>
2022-02-19 11:11:47 -0600

    initial resource list commit, needs better styling and linking

---
+++ src/components/ResourceLinks/ResourceLinks.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+
+// Sets the menu items
+const options = ["Resources", "Nomics", "Test Entry"];
+
+const ResourceLinks = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const open = Boolean(anchorEl);
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <List component="nav" aria-label="Resources" sx={{ bgcolor: "white" }}>
+        <ListItem
+          // setting bgcolor here causes a slight strobe color effect on menu click
+          button
+          id="lock-button"
+          aria-haspopup="listbox"
+          aria-controls="lock-menu"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClickListItem}
+        >
+          <ListItemText secondary={options[selectedIndex]} />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "lock-button",
+          role: "listbox",
+        }}
+      >
+        {options.map((option, index) => (
+          <MenuItem
+            sx={{ color: "white", background: "black" }} // sets text color for menu items
+            key={option}
+            disabled={index === 0}
+            selected={index === selectedIndex}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
+
+export default React.memo(ResourceLinks);
