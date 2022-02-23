38ef80de339c2ce37f2eebc6add579c2d8025898
scrappydew <scrappy@mm.st>
2022-02-23 12:30:18 -0600

    add list of voting sources which are included/excluded from weight

--- src/views/Vote/Vote.tsx
+++ src/views/Vote/Vote.tsx
@@ -6,6 +6,58 @@ import VotingBooth from "components/VotingBooth";
 import { getDeployedVotingContracts } from "utils/voting";
 import Typography from "@mui/material/Typography";
 import { useWallet } from "use-wallet";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Grid from "@mui/material/Grid";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+const SmallCheck = () => {
+  return <CheckBoxIcon fontSize="small" />;
+};
+
+const SmallCancel = () => {
+  return <CancelIcon fontSize="small" />;
+};
+
+const VoteSources = () => {
+  return (
+    <>
+      <Grid sx={{ display: "flex", justifyContent: "center" }} container direction="row">
+        <Grid item>
+          <Typography sx={{ mt: "10px" }} variant="h6">
+            Weight currently includes:
+          </Typography>
+          <List sx={{ justifyContent: "center" }} dense={true}>
+            <ListItem>
+              <SmallCheck />
+              INK holdings
+            </ListItem>
+            <ListItem>
+              <SmallCheck />
+              INK held in farming contracts
+            </ListItem>
+            <ListItem>
+              <SmallCheck />
+              INK provided as LP
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid sx={{ marginLeft: "80px" }} item>
+          <Typography sx={{ mt: "10px" }} variant="h6">
+            Weight <u>DOES NOT</u> include:
+          </Typography>
+          <List>
+            <ListItem>
+              <SmallCancel />
+              Un-harvested farmed INK
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 const Instructions = () => {
   return (
@@ -19,6 +71,7 @@ const Instructions = () => {
         works similar to a snapshot in that everyone's weight is calculated from the same block, and that block is known to everyone. This way, users
         aren't able to manipulate their voting weight during the course of the vote.
       </Typography>
+      <VoteSources />
     </>
   );
 };
