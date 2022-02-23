406021e70dd500f39af12dbdc2a3ee987c1ab987
scrappydew <scrappy@mm.st>
2022-02-23 11:57:43 -0600

    update explainer text, release notes and package.json

--- src/views/Vote/Vote.tsx
+++ src/views/Vote/Vote.tsx
@@ -13,11 +13,11 @@ const Instructions = () => {
       <Typography variant="h4">How does it work?</Typography>
 
       <Typography sx={{ width: "80%", mt: "5px" }}>
-        After connecting your wallet you will see ballots populated on this page; these ballots are from the Ubiq blockchain. Each ballot is a separate smart contract and that contract contains the details of
-        the ballot (description, summary, vote options, etc...). Those details are read from the contract and displayed on the frontend. Voting weight
-        is calculated based on the "start_block" value in the ballot contract. It works similar to a snapshot in that everyone's weight is calculated
-        from the same block, and that block is known to everyone. This way, users aren't able to manipulate their voting weight during the course of
-        the vote.
+        After connecting your wallet you will see ballots populated on this page; these ballots are from the Ubiq blockchain. Each ballot is a
+        separate smart contract and that contract contains the details of the ballot (description, summary, vote options, etc...). Those details are
+        read from the contract and displayed on the frontend. Voting weight is calculated based on the "start_block" value in the ballot contract. It
+        works similar to a snapshot in that everyone's weight is calculated from the same block, and that block is known to everyone. This way, users
+        aren't able to manipulate their voting weight during the course of the vote.
       </Typography>
     </>
   );
