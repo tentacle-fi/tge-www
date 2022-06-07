ce9d675b8047b04658a2517f8339d92b8c591320
scrappydew <scrappy@mm.st>
2022-06-06 21:01:47 -0500

    replace neu with mui useTheme

--- src/components/icons/Menu.tsx
+++ src/components/icons/Menu.tsx
@@ -1,16 +1,16 @@
 import React from "react";
+import { useTheme } from "@mui/material";
 import styled from "styled-components";
-
-import { useTheme } from "react-neu";
 
 interface CloseProps {
   color?: string;
 }
 
 const Menu: React.FC<CloseProps> = ({ color }) => {
-  const { textColor } = useTheme();
+  const { typography } = useTheme();
+
   return (
-    <StyledSVG color={color ? color : textColor}>
+    <StyledSVG color={color ? color : (typography.body1.color as string)}>
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
     </StyledSVG>
@@ -20,6 +20,7 @@ const Menu: React.FC<CloseProps> = ({ color }) => {
 interface StyledSVGProps {
   color: string;
 }
+
 const StyledSVG = styled.svg.attrs(() => ({
   height: "24",
   viewBox: "0 0 24 24",
